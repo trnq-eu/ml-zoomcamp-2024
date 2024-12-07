e05248d9f07b67d98b4040a5acc3f2689cfc9521
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2024-12-07 06:48:30 +0100

    added homework for lesson 9

---
+++ lesson 09/homework-202409/lambda_function.py
@@ -0,0 +1,43 @@
+import tflite_runtime.interpreter as tflite
+from keras_image_helper import create_preprocessor
+
+interpreter = tflite.Interpreter(model_path='clothing-model.tflite')
+interpreter.allocate_tensors()
+
+input_index = interpreter.get_input_details()[0]['index']
+output_index = interpreter.get_output_details()[0]['index']
+
+preprocessor = create_preprocessor('xception', target_size=(299, 299))
+
+# url = 'http://bit.ly/mlbookcamp-pants'
+
+
+classes = [
+    'dress',
+    'hat',
+    'longsleeve',
+    'outwear',
+    'pants',
+    'shirt',
+    'shoes',
+    'shorts',
+    'skirt',
+    't-shirt'
+]
+
+
+def predict(url):
+  X = preprocessor.from_url(url)
+
+  interpreter.set_tensor(input_index, X)
+  interpreter.invoke()
+  preds = interpreter.get_tensor(output_index)
+
+  return dict(zip(classes, preds[0]))
+
+def lambda_handler(event, context):
+  url = event('url')
+  result = predict(url)
+  return result
+
+
